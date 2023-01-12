a3bf860a1b61933efe1407ad6e538c67d5c2f767
LisaPape <lisa.pape@attempto.eu>
2023-01-12 19:08:07 +0100

    refactor template and style it with Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,115 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-cesar-encryption';
+export class AppComponent implements OnInit {
+
+  title = 'angular-cesar-cypher';
+  inputText: string = "";
+  shiftValue = "";
+  encodedText: string = "";
+  decodedText: string = "";
+  loading: boolean | undefined;
+  isLoading = false;
+  showDecodeBlock = false;
+  loadingBlock: any;
+
+
+  ngOnInit(): void {
+  }
+
+  private validateIfCharacterIsALetter(currentLetter: any) {
+    return (/[a-zA-Z]/).test(currentLetter)
+  }
+
+  clear(){
+    this.inputText = "";
+    this.shiftValue = "";
+    this.encodedText = "";
+    this.decodedText = "";
+  }
+
+  encodeInput(): void{
+    const shiftValue = Number(this.shiftValue) % 26; //edge case of input of high number
+    let alphabetLower: string[] = "abcdefghijklmnopqrstuvwxyz".split("");
+    let alphabetUpper: string[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    let encodedText = "";
+    this.isLoading = true;
+
+
+    for(let i = 0; i < this.inputText.length; i++) {
+      let currentLetter = this.inputText[i];
+
+      if (!this.validateIfCharacterIsALetter(currentLetter)) {
+        encodedText += currentLetter;
+        continue;
+      }
+
+      if(currentLetter == currentLetter.toLowerCase()) {
+        let currentIndex = alphabetLower.indexOf(currentLetter);
+        let newIndex = currentIndex + shiftValue;
+        if (newIndex > 25) newIndex = newIndex - 26;
+        if (newIndex < 0) newIndex = newIndex + 26;
+        encodedText += alphabetLower[newIndex];
+        console.log("Lower", encodedText);
+      }
+      else if(currentLetter == currentLetter.toUpperCase()) {
+        let currentIndex = alphabetUpper.indexOf(currentLetter);
+        let newIndex = currentIndex + shiftValue;
+        if (newIndex > 25) newIndex = newIndex - 26;
+        if (newIndex < 0) newIndex = newIndex + 26;
+        encodedText += alphabetUpper[newIndex];
+        console.log("Upper", encodedText);
+      }
+    }
+    this.encodedText = encodedText;
+    this.isLoading = false;
+  }
+
+  decodeInput(): void{
+    const shiftValue = Number(this.shiftValue) % 26; //edge case of input of high number
+    let alphabetLower: string[] = "abcdefghijklmnopqrstuvwxyz".split("");
+    let alphabetUpper: string[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    this.isLoading = true;
+    let decodedText = "";
+
+
+    for(let i = 0; i < this.encodedText.length; i++) {
+      let currentLetter = this.encodedText[i];
+
+      if (!this.validateIfCharacterIsALetter(currentLetter)) {
+        decodedText += currentLetter;
+        continue;
+      }
+
+      if(currentLetter == currentLetter.toLowerCase()) {
+        let currentIndex = alphabetLower.indexOf(currentLetter);
+        let newIndex = currentIndex - shiftValue;
+        if (newIndex > 25) newIndex = newIndex - 26;
+        if (newIndex < 0) newIndex = newIndex + 26;
+        decodedText += alphabetLower[newIndex];
+        console.log("Lower", decodedText);
+      }
+      else if(currentLetter == currentLetter.toUpperCase()) {
+        let currentIndex = alphabetUpper.indexOf(currentLetter);
+        let newIndex = currentIndex - shiftValue;
+        if (newIndex > 25) newIndex = newIndex - 26;
+        if (newIndex < 0) newIndex = newIndex + 26;
+        decodedText += alphabetUpper[newIndex];
+        console.log("Upper", decodedText);
+      }
+    }
+
+    this.decodedText = decodedText;
+    this.isLoading = false;
+  }
+
 }
+
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {FormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatCardModule} from "@angular/material/card";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatIconModule} from "@angular/material/icon";
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    FlexLayoutModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
